eab69235eeb6c6422a98d83879b64f2fd018a658
Dilma Sae <dilmasae@gmail.com>
2018-03-04 23:20:26 +0000

    set product image

--- src/java/persistence/ProductDB.java
+++ src/java/persistence/ProductDB.java
@@ -20,12 +20,44 @@ public class ProductDB {
         
         if(isReady) return products;
         
-        Random rand = new Random();
+        products.add(new Product("https://images.pexels.com/photos/326583/pexels-photo-326583.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Zapatitos de bebé " , "Lindos zapatitos tejidos a mano.", 35));
         
-        for(int i = 0; i < 10; i++) {
-            products.add(new Product("http://via.placeholder.com/300X300", "Titulo " + i, "Texto "+ i, rand.nextInt(50) + 1));
-        }
+        products.add(new Product("https://images.pexels.com/photos/45223/gift-made-surprise-loop-45223.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Cajas de regalo " , " Pequeñas cajas de regalo .", 3));
         
+        products.add(new Product("https://images.pexels.com/photos/533604/pexels-photo-533604.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Corazones" , "Pequeños corazones ideales para regalar", 5));
+        
+        
+        products.add(new Product("https://images.pexels.com/photos/898322/pexels-photo-898322.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Galletas decoradas" , "Galletas decoradas para regalar", 6));
+        
+        
+        products.add(new Product("https://images.pexels.com/photos/268364/pexels-photo-268364.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Cupcake decorativas" , " Cupcake de papel ideals para reagalar ", 3));
+        
+        products.add(new Product("https://images.pexels.com/photos/68558/pexels-photo-68558.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Flores de papel " , "Ideales para regalar ", 1));
+        
+        products.add(new Product("https://images.pexels.com/photos/220987/pexels-photo-220987.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Florero azul" , "Florero artesanal hecho a mano", 45));
+        
+        products.add(new Product("https://images.pexels.com/photos/255380/pexels-photo-255380.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Bola decorativa" , "lorem lorem lorem lorem", 3));
+        
+        products.add(new Product("https://images.pexels.com/photos/730919/christmas-tree-christmas-tree-decoration-730919.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+                "Estrellas de navidad" , "lorem lorem lorem lorem", 3));
+        
+        
+        
+        https://images.pexels.com/photos/533604/pexels-photo-533604.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb
+        
+        
+        /*products.add(new Product("http://via.placeholder.com/300X300", "Titulo " + i, "Texto "+ i, rand.nextInt(50) + 1));
+        products.add(new Product("http://via.placeholder.com/300X300", "Titulo " + i, "Texto "+ i, rand.nextInt(50) + 1));
+        products.add(new Product("http://via.placeholder.com/300X300", "Titulo " + i, "Texto "+ i, rand.nextInt(50) + 1));
+        */
         isReady = true;
         return ProductDB.products;
     }
